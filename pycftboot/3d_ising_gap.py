df94e80049bee4f2f3fbdb620c27cf7cc97007d5
MattDowens <mtdowens@gmail.com>
2018-06-07 10:49:37 +0100

    improvements to gapped ising

--- pycftboot/3d_ising_gap.py
+++ pycftboot/3d_ising_gap.py
@@ -2,26 +2,38 @@ import bootstrap
 import matplotlib.pyplot as plt
 import time
 import datetime
+import numpy as np
 
+#Start timing
 start_time=time.time()
 start_cpu=time.clock()
+
+#Parameters for SDPB
 bootstrap.cutoff=1e-10
 dim=3
 k_max=20
 l_max=15
 n_max=4
-m_max=3
-sig=0.52
-eps=1.2
+m_max=2
 
-table1=bootstrap.ConformalBlockTable(dim,k_max,l_max,m_max,n_max)
-table2=bootstrap.ConvolvedBlockTable(table1)
+#Initializing conformal and convolved conformal blocks
+tab1=bootstrap.ConformalBlockTable(dim,k_max,l_max,m_max,n_max)
+tab2=bootstrap.ConvolvedBlockTable(tab1)
 
-sdp=bootstrap.SDP(sig,table2)
-sdp.set_bound(0,3.0)
-sdp.add_point(0,eps)
-result=sdp.iterate()
+#Plots all allowed points in a coarse grid
 
+sig=np.arange(0.5,0.85,0.05)
+eps=np.arange(1.0,2.2,0.2)
+
+
+
+
+
+
+
+
+
+#Computing and formatting times (Can shorten this!)
 end_time=time.time()
 end_cpu=time.clock()
 run_time=end_time-start_time
@@ -31,5 +43,3 @@ run_time2=time.strftime("%H:%M:%S",run_time1)
 cpu_time1=time.gmtime(cpu_time)
 cpu_time2=time.strftime("%H:%M:%S",cpu_time1)
 print("Run time "+run_time2, "CPU time "+cpu_time2)
-print(result)
-
